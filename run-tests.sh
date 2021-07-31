2229e7ece692c3d68da4e0738ffb4cf149103342
Bas Couwenberg <sebastic@xs4all.nl>
2021-07-31 13:53:50 +0200

    Various shellcheck fixes.

--- run-tests.sh
+++ run-tests.sh
@@ -1,25 +1,38 @@
 #!/bin/sh
 
 for CMD in "php" "phpcs" "shellcheck"; do
-    which "${CMD}" > /dev/null 2>&1
-    if [ $? -ne 0 ]; then
+    if ! command -v "${CMD}" > /dev/null 2>&1; then
         echo "Error: Cannot find executable: ${CMD}"
         exit 1
     fi
 done
 
-set -e
+ERRORS=0
 
-cd "$(dirname "$0")/.."
+cd "$(dirname "$0")" || exit 1
 
 for FILE in $(find . -type f -and \( -name "*.php" -or -name "*.phtml" \) | sort); do
     php -l "${FILE}"
+    rc=$?
+    if [ $rc != 0 ]; then
+        ERRORS=$((ERRORS + rc))
+    fi
 
     phpcs --standard=PSR1,PSR2 --exclude="Generic.Files.LineLength" "${FILE}"
+    rc=$?
+    if [ $rc != 0 ]; then
+        ERRORS=$((ERRORS + rc))
+    fi
 done
 
 for FILE in $(find . -type f -and -name "*.sh" | sort); do
     shellcheck "${FILE}"
+    rc=$?
+    if [ $rc != 0 ]; then
+        ERRORS=$((ERRORS + rc))
+    fi
 done
 
-cd "${OLDPWD}"
+cd "${OLDPWD}" || exit 1
+
+exit "${ERRORS}"
